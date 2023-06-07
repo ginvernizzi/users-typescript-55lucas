da03ea9b89aa4070838039d6c91673b1e946947f
gonzalo <gonzalo.invernizzi@gmail.com>
2023-06-07 14:54:15 -0300

    modificaciones en los metodos de useMemo para ordenar y filtrar usaurios, pero no funciona el ordenar

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,61 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 import UsersList from "./components/UsersList"
-import { User, UserColumnsOrder } from "./types.d"
+import { User } from "./types.d"
 
 function App () {
   const [users, setUsers] = useState<Array<User>>([])
-  const [orderByCountry, setOrderByCountry] = useState(false)
+  const [orderByCountry, setOrderByCountry] = useState(false) //ordenar por pais
+  const [filter, setFilter] = useState('')
   const usersOriginal = useRef<User[]>([])
-  
+
   useEffect(() => {
-    fetch('https://randomuser.me/api/?results=100')
+    fetch(import.meta.env.VITE_API_URL)
       .then(res => res.json())
       .then(data => {
         setUsers(data.results)
         usersOriginal.current = data.results
       })
       .catch(error => console.log("se pudrio todo", error))
-
-
   }, [])
 
   const assignOrderByCOuntry = () => {
-    console.log("ordernar por pais")
     setOrderByCountry(!orderByCountry)
   }
 
-  const filterByCountry = (e) => {
-    console.log("filtro por pais")
-    return ([...users].filter((user) => { return user.location.country.toLowerCase().startsWith(e.target.value.toLowerCase()) }))
+  ///////////////////////
+  const filterByCountry = useMemo(() => {
+    console.log("filtrar por pais, term", filter)
+    if (filter !== '' && filter.length >= 1) {
+      const userWithFilter = [...users].filter((u) => u.location.country.toLowerCase().startsWith(filter.toLocaleLowerCase()))
+      if (userWithFilter === undefined) {
+        return users
+      } else {
+        return userWithFilter
+      }
+    } else {
+      return users
+    }
+  }, [filter, users])
+
+  const orderUserByCountry = useMemo(() => {
+    console.log("ordernar por pais")
+    if (orderByCountry) {
+      return filterByCountry.sort((a: User, b: User) => a.location.country.localeCompare(b.location.country))
+    } else {
+      return filterByCountry
+    }
+  }, [orderByCountry, filterByCountry])
+  ///////////////////////
+
+
+  const enableFilterByCountry = (e) => {
+    console.log("seteo filter")
+    setFilter(e.target.value)
   }
 
   const deleteUser = (id: string) => {
     console.log("Delete user")
     setUsers(users.filter((u) => u.login.uuid !== id))
-  }
-
-  const orderUserByCountry = () => {
-    if (orderByCountry) {
-      return [...users].sort((a: User, b: User) => a.location.country.localeCompare(b.location.country))
-    } else {
-      return users
-    }
   }
 
   const resetUsers = () => {
@@ -65,12 +81,12 @@ function App () {
   return (
     <>
       <h1>Prueba tecnica</h1>
-      <UsersList users={orderUserByCountry()}
+      <UsersList users={orderUserByCountry}
         assignOrderByCOuntry={assignOrderByCOuntry}
         orderByCountry={orderByCountry}
         deleteUser={deleteUser}
         resetUsers={resetUsers}
-        filterByCountry={filterByCountry} />
+        filterByCountry={enableFilterByCountry} />
     </>
   )
 }
